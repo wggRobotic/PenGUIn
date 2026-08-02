36e865d996af53f3f93aaca3cfa3b73a47b863ac
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-02 23:38:08 +0200

    Added a list for the nodes

--- frontend/lib/tabs/nodes_tab.dart
+++ frontend/lib/tabs/nodes_tab.dart
@@ -1,11 +1,39 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class NodesTab extends StatelessWidget{
   const NodesTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Placeholder();
+    List<String> placeholder = ["test1", "test2", "test3", "test4", "..."];
+    final theme = Theme.of(context).colorScheme;
+
+    // Display a simple list of available nodes
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.separated(
+        itemCount: placeholder.length,
+        separatorBuilder: (context, index) => const SizedBox(height: 8),
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(placeholder[index]),
+            tileColor: theme.surfaceContainer,
+            leading: IconButton(
+              icon: Icon(Icons.play_circle_outlined),
+              onPressed: () {},
+            ),
+            selectedTileColor: theme.secondaryContainer,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+            contentPadding: EdgeInsets.all(4.0),
+            selected: true,
+          );
+        },
+      ),
+    );
   }
 }
+
+// TODO: Implement selection
+// TODO: Launch a node
+//        => Provider handling selection state
+//        => Datamodell defining a node (make sur eit matches the JSON)
