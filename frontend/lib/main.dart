5b49b565c12ddde38895b17867014a2323d31417
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-02 22:16:11 +0200

    Implement the new theme

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,36 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PenGUIn());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PenGUIn extends StatelessWidget {
+  const PenGUIn({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = TextTheme();
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'PenGUIn',
+      // Define the color theme of the frontend
+      theme: PenGUInTheme(textTheme).theme(PenGUInTheme.lightScheme()),
+      darkTheme: PenGUInTheme(textTheme).theme(PenGUInTheme.darkScheme()),
+      highContrastTheme: PenGUInTheme(textTheme).theme(PenGUInTheme.lightHighContrastScheme()),
+      highContrastDarkTheme: PenGUInTheme(textTheme).theme(PenGUInTheme.darkHighContrastScheme()),
+      home: const MyHomePage(title: 'PenGUIn'),
     );
   }
 }
