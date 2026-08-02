5cef801b02ff5014a7f4fe847f67782888db6c43
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-02 22:51:40 +0200

    Added tabs to navigate between analytics and nodes

---
+++ frontend/lib/tabs/analytics_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AnalyticsTab extends StatelessWidget{
+  const AnalyticsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Placeholder();
+  }
+}
---
+++ frontend/lib/tabs/nodes_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class NodesTab extends StatelessWidget{
+  const NodesTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Placeholder();
+  }
+}
