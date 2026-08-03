a47c1187c49e640a80a843cd603e22f3313d54b5
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-03 14:09:14 +0200

    Read the nodes config file and make sure it exists

---
+++ frontend/lib/config/nodes_config_handler.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:frontend/datamodells.dart';
+import 'package:path/path.dart' as path;
+
+class NodesConfigHandler {
+
+  // Get the path of the whole config file
+  String getRelativeConfigPath() {
+    final exePath = File(Platform.resolvedExecutable).absolute.path;
+    final baseDirectory = path.dirname(exePath);
+
+    return path.join(baseDirectory, "config", "nodes_config.json");
+  }
+
+  // Ensure the config exists
+  Future<void> ensureConfigExists(String configPath) async {
+    final file = File(configPath);
+
+    // Move on if the config exists
+    if (await file.exists()) return;
+
+    // Copy the config from the assets
+    const assetPath = 'assets/config/nodes_config.json';
+    final defaultJson = await rootBundle.loadString(assetPath);
+
+    await file.parent.create(recursive: true);
+    await file.writeAsString(defaultJson, flush: true);
+  }
+
+  // Read the config
+  Future<List<NodeDatamodell>> applyNodeConfiguration() async {
+    final configPath = getRelativeConfigPath();
+
+    // Make sure it exists
+    await ensureConfigExists(configPath);
+
+    // Get the JSON config
+    String jsonConfig = await File(configPath).readAsString();
+
+    // Parse it to the datamodell and return it
+    Map<String, dynamic> decodedConfig = jsonDecode(jsonConfig) as Map<String, dynamic>;
+    List<dynamic> nodesList = (decodedConfig['nodes'] as List<dynamic>? ?? []);
+    return nodesList.map((e) {
+      final map = e as Map<String, dynamic>;
+
+      return NodeDatamodell(
+        name: (map['name'] as String?) ?? 'No name provided',
+        description: (map['description'] as String?) ?? '',
+        documentationLink: (map['documentationLink'] as String?) ?? '',
+      );
+    }).toList();
+  }
+}
