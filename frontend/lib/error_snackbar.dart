85528f0327fb379ae0618d0e2c1bccf414fca088
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-03 14:51:31 +0200

    Added SnackBar to display errors

---
+++ frontend/lib/error_snackbar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ErrorSnackbar{
+  const ErrorSnackbar();
+
+  SnackBar buildErrorSnackBar({
+    required BuildContext context,
+    required String error,
+  }) {
+    final theme = Theme.of(context);
+
+    return SnackBar(
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: (context.mounted)
+        ? theme.colorScheme.onErrorContainer
+        : Color(0xff250700),
+      elevation: 6.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      dismissDirection: DismissDirection.horizontal,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Title
+          Text(
+            "An error occurred",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: (context.mounted)
+                ? theme.colorScheme.surface
+                : Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 6),
+          Text(
+            error,
+            style: TextStyle(
+              fontSize: 14,
+              color: (context.mounted)
+                ? theme.colorScheme.surface
+                : Colors.grey.withAlpha(200),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
