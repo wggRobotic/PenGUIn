b5267d9fb380267ebdc8d058d18c7970c18ce07a
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-03 23:47:30 +0200

    Run multiple nodes at once

--- penguin_controll_package/src/penGUIn_controller.cpp
+++ penguin_controll_package/src/penGUIn_controller.cpp
@@ -1,5 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "interface_package/msg/single.hpp"
 #include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -11,7 +11,8 @@ using namespace std::placeholders;
 class PenGUInController : public rclcpp::Node {
 public:
     PenGUInController() : Node("penguin_controller") {
-        launch_single_subscriber = this->create_subscription<std_msgs::msg::String>(
+        // Subscribe to a topic and launch a node
+        launch_single_subscriber = this->create_subscription<interface_package::msg::Single>(
             "penGUIn/start_single",
             10,
             std::bind(&PenGUInController::launchSingleNodeCallBack, this, std::placeholders::_1)
@@ -20,32 +21,49 @@ public:
     }
 
 private:
-    void launchSingleNodeCallBack(const std_msgs::msg::String::SharedPtr msg) {
-        const std::string cmd = msg->data;
+    struct ProcInfo {
+        pid_t pid;
+        bool alive;
+    };
 
-        pid_t pid = fork();
-        if (pid < 0) {
-            RCLCPP_INFO(this->get_logger(), "ERROR: Fork failed");
+    void launchSingleNodeCallBack(const interface_package::msg::Single::SharedPtr msg) {
+        std::string cmd = msg ->cmd;
+        int id = msg->id;
+        RCLCPP_WARN(this->get_logger(), "Id: %i", id);
+
+        if (cmd.empty()) {
+            RCLCPP_WARN(this->get_logger(), "ERROR: Unprocessable values");
             return;
         }
 
-        // Run via shell
+        // Return if a prozess with the same id already exists
+        if (procs.count(id) && procs[id].alive) {
+            RCLCPP_WARN(this->get_logger(), "Prozess with id '%i' already running", id);
+            return;
+        }
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            RCLCPP_ERROR(get_logger(), "Fork failed");
+            return;
+        }
+
         if (pid == 0) {
+            // Apply a certain process group
+            setpgid(0, 0);
+
             const char* argv[] = {"bash", "-lc", cmd.c_str(), nullptr};
             execv("/bin/bash", (char* const*)argv);
             _exit(127);
         }
 
-        // Wait for the result
-        int status = 0;
-        (void)waitpid(pid, &status, 0);
-
-        int rc = -1;
-        if (WIFEXITED(status)) rc = WEXITSTATUS(status);
-        RCLCPP_INFO(this->get_logger(), "ros2 run exit code: %d", rc);
+        // Parent
+        procs[id] = ProcInfo{pid, true};
+        RCLCPP_INFO(get_logger(), "Started id='%i' pid=%d", id, pid);
     }
 
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr launch_single_subscriber;
+    std::unordered_map<int, ProcInfo> procs; // id -> pid
+    rclcpp::Subscription<interface_package::msg::Single>::SharedPtr launch_single_subscriber;
 };
 
 int main(int argc, char **argv) {
@@ -55,3 +73,4 @@ int main(int argc, char **argv) {
     rclcpp::shutdown();
     return 0;
 }
+
