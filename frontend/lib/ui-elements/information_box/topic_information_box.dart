5532783fc8bab0a65c421c23b8a451568c145d1e
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-06 19:54:12 +0200

    Receive and display further information

--- frontend/lib/ui-elements/information_box/topic_information_box.dart
+++ frontend/lib/ui-elements/information_box/topic_information_box.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/custom_provider.dart';
+import 'package:frontend/datamodells.dart';
+import 'package:frontend/storage/rosbridge_connector.dart';
 import 'package:frontend/ui-elements/custom_card.dart';
+import 'package:provider/provider.dart';
 
-class TopicInformationBox extends StatelessWidget{
+class TopicInformationBox extends StatefulWidget{
+  final AnalyticsDatamodell topic;
 
-  const TopicInformationBox({super.key});
+  const TopicInformationBox({
+    super.key,
+    required this.topic
+  });
+
+  @override
+  State<TopicInformationBox> createState() => _TopicInformationBoxState();
+}
+
+class _TopicInformationBoxState extends State<TopicInformationBox> {
+  final RosbridgeConnector connector = RosbridgeConnector();
+
+  @override
+  void initState() {
+    super.initState();
+    // Request the desired data
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      connector.getTopicInterface(context, widget.topic.name);
+      connector.getTopicPublishers(context, widget.topic.name);
+      connector.getTopicSubscribers(context, widget.topic.name);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final interface = context.watch<TopicInformationProvider>().interface;
+    final publisher = context.watch<TopicInformationProvider>().publisher;
+    final subscriber = context.watch<TopicInformationProvider>().subscriber;
+
+    // Display the data
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomCard(
           title: "Description:",
-          value: "TODO"
+          value: widget.topic.description
         ),
         CustomCard(
-          title: "Schema:",
-          value: "TODO"
+          title: "Interface:",
+          value: interface
         ),
         CustomCard(
           title: "Implementation:",
@@ -24,8 +55,8 @@ class TopicInformationBox extends StatelessWidget{
             spacing: 4.0,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Publisher:"),
-              Text("Subscriber:"),
+              Text("Publisher: $publisher"),
+              Text("Subscriber: $subscriber"),
             ],
           ),
         ),
