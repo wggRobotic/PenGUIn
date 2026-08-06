2610d478cb01ff63bb2910d2b9f2dc7aa3c8e9c3
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-06 23:31:49 +0200

    Display information about a certain service

--- frontend/lib/ui-elements/information_box/service_information_box.dart
+++ frontend/lib/ui-elements/information_box/service_information_box.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/custom_provider.dart';
+import 'package:frontend/datamodells.dart';
+import 'package:frontend/storage/rosbridge_connector.dart';
 import 'package:frontend/ui-elements/custom_card.dart';
+import 'package:provider/provider.dart';
 
-class ServiceInformationBox extends StatelessWidget{
+class ServiceInformationBox extends StatefulWidget{
+  final AnalyticsDatamodell service;
 
-  const ServiceInformationBox({super.key});
+  const ServiceInformationBox({
+    super.key,
+    required this.service,
+  });
+
+  @override
+  State<ServiceInformationBox> createState() => _ServiceInformationBoxState();
+}
+
+class _ServiceInformationBoxState extends State<ServiceInformationBox> {
+  final RosbridgeConnector connector = RosbridgeConnector();
+
+  @override
+  void initState() {
+    super.initState();
+    // Request the desired data
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      connector.getServiceInterface(context, widget.service.name);
+      connector.getServiceProviders(context, widget.service.name);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final provider = context.watch<ServiceInformationProvider>().provider;
+    final interface = context.watch<ServiceInformationProvider>().interface;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomCard(
           title: "Description:",
-          value: "TODO"
+          value: widget.service.description,
         ),
         CustomCard(
           title: "Provider:",
-          value: "TODO"
+          value: provider
         ),
         CustomCard(
-          title: "Schema:",
-          value: "TODO"
+          title: "Interface:",
+          value: interface
         ),
       ],
     );
