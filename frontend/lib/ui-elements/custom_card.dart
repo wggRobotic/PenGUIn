707a9f28fd837ef2bf289d5a3c0268a9c8e67f33
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-08-06 15:01:23 +0200

    Custom Card to display information more organised

---
+++ frontend/lib/ui-elements/custom_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget{
+  final String title;
+  final String value;
+  final Widget? data;
+
+  const CustomCard({
+    super.key,
+    required this.title,
+    this.value = "",
+    this.data,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return Card(
+      color: theme.surfaceContainerHigh,
+      surfaceTintColor: theme.primary,
+      margin: EdgeInsets.only(left: 8.0, top: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: 468,
+          child: Row(
+            spacing: 8.0,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title),
+              data == null
+                ? Expanded(
+                  child: Text(
+                    value,
+                    softWrap: true,
+                  ),
+                )
+              : data!,
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
